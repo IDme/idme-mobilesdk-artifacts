cab13370a9cc4cea676efc892ae7dafaaa01f466
ID.me Github Service <94928905+service-idme-github@users.noreply.github.com>
2026-03-09 14:44:25 -0700

    Update Package.swift for release iOS-v0.9.0 (#3)
    Co-authored-by: GitHub Action <action@github.com>

--- Package.swift
+++ Package.swift
@@ -18,17 +18,18 @@ let package = Package(
         // Consumers don't need to add these - SPM resolves them automatically
         .package(url: "https://github.com/openid/AppAuth-iOS.git", from: "2.0.0"),
         .package(url: "https://github.com/auth0/JWTDecode.swift", from: "3.3.0"),
-        .package(url: "https://github.com/Veriff/veriff-ios-spm", from: "8.3.0"),
-        .package(url: "https://github.com/Veriff/veriff-nfc-ios-spm", from: "8.5.0"),
+        .package(url: "https://github.com/Veriff/veriff-ios-spm", from: "9.0.0"),
+        .package(url: "https://github.com/Veriff/veriff-nfc-ios-spm", from: "9.0.0"),
         .package(url: "https://github.com/krzyzanowskim/OpenSSL-Package.git", exact: "3.3.3001"),
         .package(url: "https://github.com/open-telemetry/opentelemetry-swift", from: "1.17.1"),
+        .package(url: "https://github.com/iProov/ios-spm.git", from: "13.0.0"),
     ],
     targets: [
         // The actual binary XCFramework
         .binaryTarget(
             name: "IDmeSDK",
-            url: "https://github.com/IDme/idme-mobilesdk-artifacts/releases/download/iOS-v0.8.0/IDmeSDK-v0.8.0.xcframework.zip",
-            checksum: "442add6ae6009193b30cb1f9b226adbc3ba025f7d12cd6f267f1a1f26e64bd2c"
+            url: "https://github.com/IDme/idme-mobilesdk-artifacts/releases/download/iOS-v0.9.0/IDmeSDK-v0.9.0.xcframework.zip",
+            checksum: "811afa3d082698aef0fa9a0df2ce95900a8bca066ecdcf8574293bf5ef4235ae"
         ),
 
         // Wrapper target that ties the binary with all dependencies
@@ -47,6 +48,7 @@ let package = Package(
                 .product(name: "OpenSSL", package: "OpenSSL-Package"),
                 .product(name: "Veriff", package: "veriff-ios-spm"),
                 .product(name: "VeriffNFC", package: "veriff-nfc-ios-spm"),
+                .product(name: "iProov", package: "ios-spm"),
             ],
             path: "Sources/IDmeSDKWrapper"
         )
